498731b31bbdcfe48284b9b854c77678a1cc98a7
Michiel Borkent <michielborkent@gmail.com>
2020-01-01 22:59:06 +0100

    Upgrade clj-kondo to ignore defprotocol

--- src/carve/impl.clj
+++ src/carve/impl.clj
@@ -57,8 +57,8 @@
 (defn ignore-by-definer? [defined-by]
   (or (= 'clojure.core/deftype defined-by)
       (= 'clojure.core/defrecord defined-by)
-      (= 'clojure.core/definterface defined-by)
-      (= 'clojure.core/defprotocol defined-by)))
+      (= 'clojure.core/defprotocol defined-by)
+      (= 'clojure.core/definterface defined-by)))
 
 (defn carve [file vs {:keys [:out-dir :api-namespaces]
                       :as opts}]
