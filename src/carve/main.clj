6bcb222dec4c01f9acbb453e9f4eb69c4b5d09f7
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-02-04 11:23:59 +0000

    add some initial options validation, closes #12
    - use spec to validate paths (checking that they are all strings)
    - check that all the paths exist
    - add expound for a nicer output
    - make `paths` a required argument

--- src/carve/main.clj
+++ src/carve/main.clj
@@ -1,14 +1,34 @@
 (ns carve.main
   (:require
+   [clojure.java.io :as io]
+   [clojure.spec.alpha :as s]
    [carve.impl :as impl]
-   [clojure.edn :as edn]))
+   [clojure.edn :as edn]
+   [expound.alpha :as expound]))
 
 (set! *warn-on-reflection* true)
+(set! s/*explain-out* expound/printer)
+(s/check-asserts true)
+
+(s/def ::paths (s/coll-of string?))
+(s/def ::opts (s/keys :req-un [::paths]))
+
+(defn- valid-path?
+  [p]
+  (.exists (io/file p)))
+
+(defn- validate-opts!
+  "Validate options throwing an exception if they don't validate"
+  [{:keys [paths] :as opts}]
+  (s/assert ::opts opts)
+  (when-not (every? valid-path? paths)
+    (throw (ex-info "Path not found" {:paths paths}))))
 
 (defn -main [& [flag opts & _args]]
   (when-not (= "--opts" flag)
     (throw (ex-info (str "Unrecognized option: " flag) {:flag flag})))
   (let [opts (edn/read-string opts)
+        _ (validate-opts! opts)
         report (impl/run! opts)]
     (when-let [r (:report opts)]
       (let [format (:format r)]
