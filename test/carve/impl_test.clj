d545fd7378ad315ccf310ca960f26b6bff45a890
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-02-07 20:06:49 +0000

    use the current output as the expected output

--- test/carve/impl_test.clj
+++ test/carve/impl_test.clj
@@ -5,15 +5,44 @@
 
 (deftest impl-test []
   (testing "without aggressive"
-    (is (= '({:filename "test-resources/app/api.clj", :row 3, :col 1, :ns api, :name private-lib-function}
-             {:filename "test-resources/app/app.clj", :row 4, :col 1, :ns app, :name unused-function}
-             {:filename "test-resources/app/app.clj", :row 5, :col 1, :ns app, :name another-unused-function})
-           (impl/run! {:paths [(.getPath (io/file "test-resources" "app"))]
+    (is (= '({:filename "test-resources/app/api.clj"
+              :row      3
+              :col      1
+              :ns       api
+              :name     private-lib-function}
+             {:filename "test-resources/app/app.clj"
+              :row      4
+              :col      1
+              :ns       app
+              :name     unused-function}
+             {:filename "test-resources/app/app.clj"
+              :row      5
+              :col      1
+              :ns       app
+              :name     another-unused-function})
+           (impl/run! {:paths       [(.getPath (io/file "test-resources" "app"))]
                        :ignore-vars ['app/-main 'app/ignore-me] :api-namespaces ['api] :report true}))))
+
   (testing "with aggressive"
-    (is (= '[{:filename "test-resources/app/app.clj", :row 4, :col 1, :ns app, :name unused-function}
-             {:filename "test-resources/app/app.clj", :row 5, :col 1, :ns app, :name another-unused-function}
-             {:filename "test-resources/app/api.clj", :row 3, :col 1, :ns api, :name private-lib-function}
-             {:filename "test-resources/app/app.clj", :row 3, :col 1, :ns app, :name only-used-by-unused-function}]
-           (impl/run! {:paths [(.getPath (io/file "test-resources" "app"))]
+    (is (= '[{:filename "test-resources/app/api.clj",
+              :row      3,
+              :col      1,
+              :ns       api,
+              :name     private-lib-function}
+             {:filename "test-resources/app/app.clj",
+              :row      4,
+              :col      1,
+              :ns       app,
+              :name     unused-function}
+             {:filename "test-resources/app/app.clj",
+              :row      5,
+              :col      1,
+              :ns       app,
+              :name     another-unused-function}
+             {:filename "test-resources/app/app.clj",
+              :row      9,
+              :col      1,
+              :ns       app,
+              :name     ignore-me}]
+           (impl/run! {:paths       [(.getPath (io/file "test-resources" "app"))]
                        :ignore-vars ['app/-main] :api-namespaces ['api] :report true :aggressive? true})))))
