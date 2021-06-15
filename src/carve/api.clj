fbbef6e98eaea68ec61332dc21e2053e38747305
Michiel Borkent <michielborkent@gmail.com>
2021-06-15 12:15:49 +0200

    minor

--- src/carve/api.clj
+++ src/carve/api.clj
@@ -4,7 +4,7 @@
 
 (defn print! [{:keys [:report :config]}]
   (let [format (-> config :report :format)]
-    (impl/print-report (:unused-vars report) format)))
+    (impl/print-report report format)))
 
 (defn run!
   "Returns unused vars as EDN data in :report. The entire return value
@@ -12,14 +12,10 @@
   CLI."
   ([] (run! nil))
   ([opts]
-   (let [{:keys [:report :config]} (impl/run+ opts)]
-     {:report {:unused-vars report}
-      :config config})))
+   (impl/run+ opts)))
 
 (defn report
   "Similar to run! but forces non-side-effecting non-interactive behavior."
   ([] (report {:merge-config true}))
   ([opts]
-   (let [{:keys [:report :config]} (impl/run+ (assoc opts :report true))]
-     {:report {:unused-vars report}
-      :config config})))
+   (impl/run+ (assoc opts :report true))))
